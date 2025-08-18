333c98e8810a28cc5e63c03b21332cd47175c315
Ren <renan199767@gmail.com>
2025-08-18 19:25:21 +0300

    add main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# uvicorn app.main:app --port 8000 --host 0.0.0.0
+# asynccontextmanager - создание асинхронных контекстных менеджеров. 
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from app.api.router import router as api_router
+from app.chroma.create_db import chroma_database
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await chroma_database.init()
+    app.include_router(api_router, prefix="/api", tags=["API"])
+    yield
+    await chroma_database.close()
+
+
+app = FastAPI(lifespan=lifespan)
